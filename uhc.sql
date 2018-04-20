245e95e095d01988eb135559726be1e77397e0f0
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-04-20 07:47:28 -0400

    Uppdated the database script

--- uhc.sql
+++ uhc.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS wallets(
 	address varchar(256) NOT NULL,
 	seed varchar(256) NOT NULL,
     CONSTRAINT pk_wallets PRIMARY KEY (id)
-)
+);
 
 -- REPRESENTS AN INDIVIDUAL WHO USES THE UHC SERVICE
 -- TODO: DETERMINE WHICH OF THESE FIELDS ARE MANDATORY
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS users(
     deactivation_time TIMESTAMPTZ,
     CONSTRAINT pk_users PRIMARY KEY (id),
     CONSTRAINT fk_users_wallets FOREIGN KEY (wallet_id) REFERENCES wallets(id)
-)
+);
 
 -- REPRESENTS CLAIMS ABOUT A USER
 CREATE TABLE IF NOT EXISTS user_claims(
@@ -68,8 +68,8 @@ CREATE TABLE IF NOT EXISTS user_claims(
     expiry TIMESTAMPTZ, -- WHEN PRESENT THE EXPIRATION OF THIS CLAIM
     user_id uuid NOT NULL, -- THE USER TO WHICH THE CLAIM BELONGS
     CONSTRAINT pk_user_claims PRIMARY KEY (id),
-    CONSTRAINT fk_user_claims_user FOREIGN KEY (user_id) REFERENCES users(user_id)
-)
+    CONSTRAINT fk_user_claims_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 -- REPRESENTS EXTERNAL IDENTITIES 
 CREATE TABLE IF NOT EXISTS user_identity (
@@ -80,13 +80,13 @@ CREATE TABLE IF NOT EXISTS user_identity (
     user_id uuid NOT NULL, -- THE USER TO WHICH THE IDENTITY BELONGS
     CONSTRAINT pk_user_identity PRIMARY KEY (id),
     CONSTRAINT fk_user_identity_user FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 -- GROUPS OF USERS
-CREATE TABLE IF NOT EXISTS group (
+CREATE TABLE IF NOT EXISTS groups (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
     name VARCHAR(16) NOT NULL, -- THE NAME OF THE GROUP
-    creation_time TIMETSAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
+    creation_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
     created_by UUID NOT NULL, -- THE USER WHICH CREATED THE GROUP
     updated_time TIMESTAMPTZ, -- THE TIME THAT THE OBJECT WAS UPDATED
     updated_by UUID, -- THE USER WHICH UPDATED THE OBJECT
@@ -96,14 +96,14 @@ CREATE TABLE IF NOT EXISTS group (
     CONSTRAINT fk_group_created_by FOREIGN KEY (created_by) REFERENCES users(id),
     CONSTRAINT fk_group_updated_by FOREIGN KEY (updated_by) REFERENCES users(id),
     CONSTRAINT fk_group_deactivated_by FOREIGN KEY (deactivated_by) REFERENCES users(id)
-)
+);
 
 -- USER <> GROUP ASSOCIATION
 CREATE TABLE IF NOT EXISTS user_group (
     user_id UUID NOT NULL,
     group_id UUID NOT NULL,
     CONSTRAINT pk_user_group PRIMARY KEY (user_id, group_id),
-    CONSTRAINT fk_user_group_user FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT fk_user_group_user FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT fk_user_group_group FOREIGN KEY (group_id) REFERENCES groups(id)
 );
 
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS permission_sets (
     id UUID NOT NULL DEFAULT uuid_generate_v4(),
     name VARCHAR(256) UNIQUE NOT NULL,  -- A UNIQUE NAME FOR THE PERMISSION SET
     description TEXT,  -- THE DESCRIPTION OF THE PERMISSION SET
-    creation_time TIMETSAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
+    creation_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
     created_by UUID NOT NULL, -- THE USER WHICH CREATED THE GROUP
     updated_time TIMESTAMPTZ, -- THE TIME THAT THE OBJECT WAS UPDATED
     updated_by UUID, -- THE USER WHICH UPDATED THE OBJECT
@@ -124,6 +124,7 @@ CREATE TABLE IF NOT EXISTS permission_sets (
     CONSTRAINT fk_permission_deactivated_by FOREIGN KEY (deactivated_by) REFERENCES users(id)
 );
 
+-- ASSOCIATES GROUPS TO PERMISSION SETS
 CREATE TABLE IF NOT EXISTS group_permissions (
     group_id UUID NOT NULL, 
     permission_set_id UUID NOT NULL, -- THE PERMISSION SET
@@ -135,10 +136,10 @@ CREATE TABLE IF NOT EXISTS group_permissions (
 
 -- REPRESENTS OAUTH APPLICATIONS
 CREATE TABLE IF NOT EXISTS applications (
-    id UUID NOT NULL, 
+    id UUID NOT NULL DEFAULT uuid_generate_v4(), 
     name VARCHAR(256) UNIQUE NOT NULL, -- REPRESENTS THE NAME OF THE APPLICATION
     secret VARCHAR(256) NOT NULL, -- REPRESENTS THE APPLICATION SECRET
-    creation_time TIMETSAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
+    creation_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THAT THE OBJECT WAS CREATED
     invalid_login INT NOT NULL DEFAULT 0,
     last_login TIMESTAMPTZ,
     lockout TIMESTAMPTZ, -- THE TIME THAT THE ACCOUNT IS LOCKED UNTIL
@@ -151,9 +152,9 @@ CREATE TABLE IF NOT EXISTS applications (
     CONSTRAINT fk_application_created_by FOREIGN KEY (created_by) REFERENCES users(id),
     CONSTRAINT fk_application_updated_by FOREIGN KEY (updated_by) REFERENCES users(id),
     CONSTRAINT fk_application_deactivated_by FOREIGN KEY (deactivated_by) REFERENCES users(id)    
-)
+);
 
--- REPRESENTS APPLICATION PERMISSIONS
+-- ASSOCIATES APPLICATIONS TO THEIR PERMISSIONS
 CREATE TABLE IF NOT EXISTS application_permission (
     application_id UUID NOT NULL, 
     permission_set_id UUID NOT NULL, -- THE PERMISSION SET
@@ -161,4 +162,17 @@ CREATE TABLE IF NOT EXISTS application_permission (
     CONSTRAINT pk_application_permission PRIMARY KEY (application_id, permission_set_id),
     CONSTRAINT fk_application_permission_application FOREIGN KEY (application_id) REFERENCES applications(id),
     CONSTRAINT fk_application_permission_permission_set FOREIGN KEY (permission_set_id) REFERENCES permission_sets(id)
-)
+);
+
+-- REPRESENTS A SINGLE SESSION - A SESSION IS A COMBINATION OF A USER USING AN APPLICATION 
+CREATE TABLE IF NOT EXISTS sessions (
+    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL, 
+    application_id UUID NOT NULL,
+    not_before TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- THE TIME THE SESSION STARTED
+    not_after TIMESTAMPTZ NOT NULL, -- THE EXPIRATION TIME (NOT AFTER) OF THE SESSION
+    refresh_token VARCHAR(256), -- IF THE SESSION CAN BE EXTENDED AUTOMATICALLY, THE REFRESH TOKEN TO USE
+    CONSTRAINT pk_sessions PRIMARY KEY (id),
+    CONSTRAINT fk_sessions_users FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_sessions_application FOREIGN KEY (application_id) REFERENCES applications(id)
+);
