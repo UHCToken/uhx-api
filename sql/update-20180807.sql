d55ca193a9953088a7409736c26762538f3abcb3
Tom White <thomas.white5@mohawkcollege.ca>
2018-08-10 12:53:03 -0400

    Change table col names

--- sql/update-20180807.sql
+++ sql/update-20180807.sql
@@ -2,19 +2,19 @@
 --  * Adding chat_room and chat_message tables
 
 CREATE TABLE chat_room (
-  c_id varchar(255) NOT NULL,
-  c_title text NOT NULL,
-  c_providerId text NOT NULL,
-  c_patientId text NOT NULL,
-  PRIMARY KEY (c_id)
+  id varchar(255) NOT NULL,
+  title text NOT NULL,
+  providerId text NOT NULL,
+  patientId text NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE chat_message (
-  cm_id varchar(255) NOT NULL,
-  cm_chatroom_id varchar(255) NOT NULL,
-  cm_author text NOT NULL,
-  cm_datesent timestamp NOT NULL,
-  cm_viewedstatus varchar(20) NOT NULL,
-  cm_body varchar(500),
+  id varchar(255) NOT NULL,
+  chatroom_id varchar(255) NOT NULL,
+  author text NOT NULL,
+  datesent timestamp NOT NULL,
+  viewedstatus varchar(20) NOT NULL,
+  body varchar(500),
   PRIMARY KEY (cm_id)
 );
