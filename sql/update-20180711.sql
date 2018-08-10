e51a0bfceb36fbe904c85228e2ccfdd3cffa46de
Rory Yendt <rory.yendt@mohawkcollege.ca>
2018-08-10 11:56:37 -0400

    Care Plans

--- sql/update-20180711.sql
+++ sql/update-20180711.sql
@@ -23,10 +23,12 @@ CREATE TABLE IF NOT EXISTS care_plans (
     info VARCHAR(256),
     creation_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by UUID NOT NULL,
+    total numeric(20,7) NOT NULL,
     completion_time TIMESTAMPTZ,
     updated_time TIMESTAMPTZ, 
     updated_by UUID,
     care_relationship_id UUID NOT NULL,
+    status varchar(256) NOT NULL,
     CONSTRAINT pk_care_plan PRIMARY KEY (id),
     CONSTRAINT fk_care_relationship FOREIGN KEY (care_relationship_id) REFERENCES care_relationships(id),
     CONSTRAINT fk_updated_by FOREIGN KEY (updated_by) REFERENCES users(id),
@@ -45,7 +47,7 @@ CREATE TABLE IF NOT EXISTS care_services (
     CONSTRAINT pk_care_service_id PRIMARY KEY (id),
     CONSTRAINT fk_care_plan FOREIGN KEY (care_plan_id) REFERENCES care_plans(id),
     CONSTRAINT fk_care_service_created_by FOREIGN KEY (created_by) REFERENCES users(id)
-)
+);
 
 INSERT INTO permission_sets (id, name, description, created_by) VALUES ('3b0bc9c5-3a4a-4903-ade5-f140519d917e', 'invoice', 'Access to the INVOICE resource', '3c673456-23b1-4263-9deb-df46770852c9');
 INSERT INTO group_permissions (group_id, permission_set_id, acl_flags) VALUES ('330d2fb4-ba61-4b48-a0a1-8162a4708e96', '3b0bc9c5-3a4a-4903-ade5-f140519d917e', 22);
