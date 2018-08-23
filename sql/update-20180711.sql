2a128df7c83df7bac9f73db5e88ab95f292000cf
Rory Yendt <rory.yendt@mohawkcollege.ca>
2018-08-23 14:12:09 -0400

    Revert "Removal of deprecated permission script"
    This reverts commit 3c9ef84bc4cd7b61cd479e791d3ce65b241a61e3.

--- sql/update-20180711.sql
+++ sql/update-20180711.sql
@@ -50,3 +50,7 @@ CREATE TABLE IF NOT EXISTS care_services (
     CONSTRAINT fk_care_plan FOREIGN KEY (care_plan_id) REFERENCES care_plans(id),
     CONSTRAINT fk_care_service_created_by FOREIGN KEY (created_by) REFERENCES users(id)
 );
+
+INSERT INTO permission_sets (id, name, description, created_by) VALUES ('3b0bc9c5-3a4a-4903-ade5-f140519d917e', 'invoice', 'Access to the INVOICE resource', '3c673456-23b1-4263-9deb-df46770852c9');
+INSERT INTO group_permissions (group_id, permission_set_id, acl_flags) VALUES ('330d2fb4-ba61-4b48-a0a1-8162a4708e96', '3b0bc9c5-3a4a-4903-ade5-f140519d917e', 22);
+INSERT INTO group_permissions (group_id, permission_set_id, acl_flags) VALUES ('044894bd-084e-47bb-9428-dbd80277614a', '3b0bc9c5-3a4a-4903-ade5-f140519d917e', 15);
