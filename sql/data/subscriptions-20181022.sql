3f9e18daf871faa19aadcae274b079f75198b797
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2018-10-23 13:05:14 -0400

    - Start for sample data for subscriptions

---
+++ sql/data/subscriptions-20181022.sql
@@ -0,0 +1,158 @@
+-- Created On: 2018-10-22
+-- Generates sample data for subscriptions related to Karis, Teladoc
+
+-- Offering @ 51a3b344-daa9-476f-b044-bb98f60bc21a (1 Month)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-09-30");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, null, "2018-09-01", "2018-10-01", false, "2018-09-30");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, "2018-10-15", "2018-09-15", "2018-10-14", true, "2018-10-14");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, null, "2018-09-23", "2018-10-23", false, "2018-10-22");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, "2018-11-22", "2018-10-22", null, true, "2018-11-21");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, null, "2018-10-23", null, false, "2018-11-22");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, "2018-09-30", "2018-08-30", "2018-09-29", true, "2018-09-29");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "51a3b344-daa9-476f-b044-bb98f60bc21a", ?, null, "2018-09-01", "2018-10-01", false, "2018-09-30");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 561455e8-6706-44b0-b1e1-9c3b27a1726c (12 Month)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "561455e8-6706-44b0-b1e1-9c3b27a1726c", ?, "2018-09-23", "2017-09-23", "2018-09-22", true, "2018-09-22");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "561455e8-6706-44b0-b1e1-9c3b27a1726c", ?, null, "2017-09-23", "2018-09-23", false, "2018-09-22");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 88c30f5f-0fe1-44f6-9b77-882080feb17f (6 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "88c30f5f-0fe1-44f6-9b77-882080feb17f", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "88c30f5f-0fe1-44f6-9b77-882080feb17f", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 1bb91334-1d68-493f-b165-c4ee798c85d7 (4 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "1bb91334-1d68-493f-b165-c4ee798c85d7", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "1bb91334-1d68-493f-b165-c4ee798c85d7", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ e007d801-128b-4109-8e53-95d6f489f910 (4 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "e007d801-128b-4109-8e53-95d6f489f910", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "e007d801-128b-4109-8e53-95d6f489f910", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ ae0a9ca3-1870-4eb1-a84f-03a64f1232bf (12 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "ae0a9ca3-1870-4eb1-a84f-03a64f1232bf", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "ae0a9ca3-1870-4eb1-a84f-03a64f1232bf", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 0bb5cff3-da37-4be5-96ba-3780b7093e55 (1 Month)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "0bb5cff3-da37-4be5-96ba-3780b7093e55", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "0bb5cff3-da37-4be5-96ba-3780b7093e55", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 1f5d02bc-1b6b-4217-a4ec-d1ed2412e283 (4 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "1f5d02bc-1b6b-4217-a4ec-d1ed2412e283", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "1f5d02bc-1b6b-4217-a4ec-d1ed2412e283", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
+
+-- Offering @ 8a73d848-97b0-4715-9b6d-dd394c36c0f6 (12 Months)
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "8a73d848-97b0-4715-9b6d-dd394c36c0f6", ?, "2018-10-01", "2018-09-01", "2018-10-01", true, "2018-10-01");
+
+INSERT INTO subscriptions
+    (id, offering_id, patient_id, date_next_payment, date_subscribed, date_terminated, auto_renew, date_expired)
+VALUES 
+    (?, "8a73d848-97b0-4715-9b6d-dd394c36c0f6", ?, null, "2018-09-01", "2018-10-02", false, "2018-10-01");
+
+-- -------------------------------------------------------------------------------------------------------
