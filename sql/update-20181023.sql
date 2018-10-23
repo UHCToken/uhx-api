dd90a90e3e0e1909ef0202b2206e328872480cee
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2018-10-23 16:33:50 -0400

    - Fix for reference to message author in chat_message

--- sql/update-20181023.sql
+++ sql/update-20181023.sql
@@ -1,5 +1,7 @@
 -- UPDATE: 2018-10-23
 -- Fix to build link between chat rooms and patients/providers
+-- Cast of authorId to UUID in chat_message
+-- author_types to indicate message source in chat_message
 
 ALTER TABLE chat_room
     ALTER COLUMN providerId TYPE uuid
@@ -24,3 +26,34 @@ ALTER TABLE chat_room
    ADD CONSTRAINT fk_patient_id
    FOREIGN KEY (patient_id) 
    REFERENCES patients(id);
+
+ALTER TABLE chat_message
+    ALTER COLUMN authorId TYPE uuid
+	USING authorid::uuid;
+
+ALTER TABLE chat_message 
+    RENAME COLUMN authorId TO author_id;
+
+CREATE TABLE IF NOT EXISTS author_types (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    type_name varchar(256) NOT NULL,
+    creation_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_author_types PRIMARY KEY (id)
+);
+
+INSERT INTO author_types 
+    (id, type_name, creation_time)
+VALUES 
+    ('862da58f-ac55-4c2d-8e0a-55f43affd844', 'patient', CURRENT_TIMESTAMP);
+
+INSERT INTO author_types 
+    (id, type_name, creation_time) 
+VALUES 
+    ('371ab4f7-641d-4c4a-9b33-cf3b31ee1a9c', 'provider', CURRENT_TIMESTAMP);
+
+ALTER TABLE chat_message ADD COLUMN author_type_id uuid;
+
+ALTER TABLE chat_message 
+   ADD CONSTRAINT fk_author_type
+   FOREIGN KEY (author_type_id) 
+   REFERENCES author_types(id);
