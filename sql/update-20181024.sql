59e4f102f39c336cc16f01871e57bbe20b951fb6
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2018-10-24 09:37:21 -0400

    - Fix for FK reference to currency in offerings, now links to assets

---
+++ sql/update-20181024.sql
@@ -0,0 +1,14 @@
+-- UPDATE: 2018-10-24
+-- Fix to build link between offerings and assets
+
+ALTER TABLE offerings DROP CONSTRAINT fk_offerings_currency;
+
+ALTER TABLE offerings 
+    RENAME COLUMN currency_id TO asset_id;
+
+ALTER TABLE offerings 
+   ADD CONSTRAINT fk_offerings_assets
+   FOREIGN KEY (asset_id) 
+   REFERENCES assets(id);
+
+DROP TABLE IF EXISTS currencies;
